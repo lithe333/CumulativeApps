50b4c90e7a2ddabb1792d7c46d2b968b0098a1e6
lithe333 <lithe333@users.noreply.github.com>
2020-10-19 11:17:06 +1100

    Included App 001 and 002
    Included App 001 and 002
    
    001MagicEightball
    002ToDoList

--- 003CumulativeApplications/app/src/main/java/com/example/a003cumulativeapplications/Activity001.java
+++ 003CumulativeApplications/app/src/main/java/com/example/a003cumulativeapplications/Activity001.java
@@ -1,18 +1,72 @@
 package com.example.a003cumulativeapplications;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.jetbrains.annotations.Nullable;
+import java.util.Random;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 //import android.support.annotation.Nullable;
 
-public class Activity001 extends AppCompatActivity {
+public class Activity001 extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
 
+    //setting up variables
+    private ImageView eightballIV;
+    private TextView answerTV;
+    private String[] answerArray = {"It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes",
+            "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+            "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it",
+            "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
+
+    //main method - see link RE Override https://beginnersbook.com/2014/01/method-overriding-in-java-with-example/
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity001);
 
+        //connecting the variables to previous code by id
+        eightballIV = findViewById(R.id.a1_btn);
+        answerTV = findViewById(R.id.a1_answer);
+
+        //adding a delay
+        eightballIV.setOnTouchListener(this);
+
+        //calling the btn method
+        eightballIV.setOnClickListener(this);
+
+
+    }
+
+    //on touch method
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        //delay answer
+        answerTV.setText("..asking...");
+        return false;
+    }
+
+    //btn click method
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.a1_btn:
+                //adding delay
+                //not sure why TimeUnit.SECONDS.sleep(1); wouldn't compile
+                try {
+                    SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //random answer
+                int rand = new Random().nextInt(answerArray.length);
+                answerTV.setText(answerArray[rand]);
+                break;
+        }
     }
 }
--- 003CumulativeApplications/app/src/main/java/com/example/a003cumulativeapplications/Activity002.java
+++ 003CumulativeApplications/app/src/main/java/com/example/a003cumulativeapplications/Activity002.java
@@ -1,18 +1,76 @@
 package com.example.a003cumulativeapplications;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.jetbrains.annotations.Nullable;
+import java.util.ArrayList;
+
 //import android.support.annotation.Nullable;
 
-public class Activity002 extends AppCompatActivity {
+public class Activity002 extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
+    // Initialising the variables
+    private EditText itemET;
+    private Button btn;
+    private ListView itemsList;
+
+    // variables for reading and writings into the list
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
+
+    // associate the variables when opening the app
+    // reading the data will store the list from previous run
+    // video mentioned that adapter is to help read/write from arrays
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity002);
 
+        itemET = findViewById(R.id.a2_item_edit_text);
+        btn = findViewById(R.id.a2_add_btn);
+        itemsList = findViewById((R.id.a2_items_list));
+
+        items = A2FileHelper.readData(this);
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
+        itemsList.setAdapter(adapter);
+
+        btn.setOnClickListener(this);
+        itemsList.setOnItemClickListener(this);
+    }
+
+    // when button is clicked, we add an item to list via adaptor and FileHelper
+    // adaptor adds another any string to the list
+    // FileHelper updates the file
+    // after storing the entry we also clear the textbox
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.a2_add_btn:
+                String itemEnterred = itemET.getText().toString();
+                adapter.add(itemEnterred);
+                itemET.setText("");
+                A2FileHelper.writeData(items, this);
+                Toast.makeText(this, "Item Added", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    // when list item is clicked, we remove the item
+    // but it didn't update file so we need to use FileHelper.writedata again.
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        items.remove(position);
+        adapter.notifyDataSetChanged();
+        A2FileHelper.writeData(items,this);
+        Toast.makeText(this, "Item Removed", Toast.LENGTH_SHORT).show();
     }
 }
